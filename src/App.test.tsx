5639d405fcccd38749c06a61210abb286e73335b
Angela Melinda <angela.melinda@gmail.com>
2020-01-19 17:06:55 +0700

    Change App Unit Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import RootReducer from "./redux/reducers";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+export function customRenderWithRedux(ui: JSX.Element, store?: any) {
+  return {
+    ...render(
+      <Provider store={store}>
+        <BrowserRouter>{ui}</BrowserRouter>
+      </Provider>
+    ),
+    store
+  };
+}
+
+test("renders pokedex page", () => {
+  const history = createMemoryHistory();
+  const route = "/";
+  history.push(route);
+
+  const { getByTestId } = customRenderWithRedux(
+    <Router history={history}>
+      <App />
+    </Router>,
+    createStore(RootReducer, applyMiddleware(thunkMiddleware))
+  );
+
+  expect(getByTestId("pokedex-page")).toBeInTheDocument();
+});
+
+test("renders pokemon-detail page", () => {
+  const history = createMemoryHistory();
+  const route = "/1";
+  history.push(route);
+
+  const { getByTestId } = customRenderWithRedux(
+    <Router history={history}>
+      <App />
+    </Router>,
+    createStore(RootReducer, applyMiddleware(thunkMiddleware))
+  );
+
+  expect(getByTestId("pokemon-detail-page")).toBeInTheDocument();
 });
